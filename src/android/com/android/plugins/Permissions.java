1cdf141192f58677e11bc3e8bdcaece4dd08ed95
Ragu Sivanandha <ragu.sivanandha@gmail.com>
2016-05-04 15:24:40 -0400

    added restart app funcitonality
    some permissions need restart, so including restart call for android

--- src/android/com/android/plugins/Permissions.java
+++ src/android/com/android/plugins/Permissions.java
@@ -1,6 +1,12 @@
 package com.android.plugins;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -10,11 +16,13 @@ import org.json.JSONObject;
 
 /**
  * Created by JasonYang on 2016/3/11.
+ * Modified by RaguS on 2016/5/05.
  */
 public class Permissions extends CordovaPlugin {
-
+    private static final String TAG = "DEBUG_RESTART_PERM";
     private static final String ACTION_HAS_PERMISSION = "hasPermission";
     private static final String ACTION_REQUEST_PERMISSION = "requestPermission";
+    private static final String ACTION_REQUEST_RESTART = "requestRestart";
 
     private static final int REQUEST_CODE_ENABLE_PERMISSION = 55433;
 
@@ -32,25 +40,32 @@ public class Permissions extends CordovaPlugin {
                 }
             });
             return true;
-        } else {
-            if (ACTION_REQUEST_PERMISSION.equals(action)) {
-                cordova.getThreadPool().execute(new Runnable() {
-                    public void run() {
-                        try {
-                            requestPermissionAction(callbackContext, args);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            JSONObject returnObj = new JSONObject();
-                            addProperty(returnObj, KEY_ERROR, ACTION_REQUEST_PERMISSION);
-                            addProperty(returnObj, KEY_MESSAGE, "Request permission has been denied.");
-                            callbackContext.error(returnObj);
-                            permissionsCallback = null;
-                        }
-                    }
-                });
-                return true;
-            }
         }
+       else if (ACTION_REQUEST_PERMISSION.equals(action)) {
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    try {
+                        requestPermissionAction(callbackContext, args);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        JSONObject returnObj = new JSONObject();
+                        addProperty(returnObj, KEY_ERROR, ACTION_REQUEST_PERMISSION);
+                        addProperty(returnObj, KEY_MESSAGE, "Request permission has been denied.");
+                        callbackContext.error(returnObj);
+                        permissionsCallback = null;
+                    }
+                }
+            });
+            return true;
+        }
+        else if (ACTION_REQUEST_RESTART.equals(action)) {
+            cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                    requestRestartAction();
+                }
+            });
+        }
+
         return false;
     }
 
@@ -92,6 +107,46 @@ public class Permissions extends CordovaPlugin {
         } else {
             permissionsCallback = callbackContext;
             cordova.requestPermission(this, REQUEST_CODE_ENABLE_PERMISSION, permission.getString(0));
+        }
+    }
+
+    private void requestRestartAction() {
+        try {
+            Context c = cordova.getActivity().getApplicationContext();
+            //check if the context is given
+            if (c != null) {
+                //fetch the packagemanager so we can get the default launch activity
+                // (you can replace this intent with any other activity if you want
+                PackageManager pm = c.getPackageManager();
+                //check if we got the PackageManager
+                if (pm != null) {
+                    //create the intent with the default start activity for your application
+                    Intent mStartActivity = pm.getLaunchIntentForPackage(
+                            c.getPackageName()
+                    );
+                    if (mStartActivity != null) {
+                        mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        //create a pending intent so the application is restarted after System.exit(0) was called.
+                        // We use an AlarmManager to call this intent in 100ms
+                        int mPendingIntentId = 223344;
+                        PendingIntent mPendingIntent = PendingIntent
+                                .getActivity(c, mPendingIntentId, mStartActivity,
+                                        PendingIntent.FLAG_CANCEL_CURRENT);
+                        AlarmManager mgr = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
+                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+                        //kill the application
+                        System.exit(0);
+                    } else {
+                        Log.e(TAG, "Not able to restart application, mStartActivity null");
+                    }
+                } else {
+                    Log.e(TAG, "Not able to restart application, PM null");
+                }
+            } else {
+                Log.e(TAG, "Was not able to restart application, Context null");
+            }
+        } catch (Exception ex) {
+            Log.e(TAG, "Was not able to restart application");
         }
     }
 
